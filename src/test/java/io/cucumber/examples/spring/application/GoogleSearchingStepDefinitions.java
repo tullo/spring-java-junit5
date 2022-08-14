42409001b139c35abdeef3eca44fed37703e30a3
Andreas Amstutz <tullo@users.noreply.github.com>
2022-08-14 19:57:54 +0200

    implemented using chrome driver

--- src/test/java/io/cucumber/examples/spring/application/GoogleSearchingStepDefinitions.java
+++ src/test/java/io/cucumber/examples/spring/application/GoogleSearchingStepDefinitions.java
@@ -1,21 +1,65 @@
 package io.cucumber.examples.spring.application;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import io.cucumber.java.en.*;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GoogleSearchingStepDefinitions {
 
+    // https://sites.google.com/chromium.org/driver/getting-started?authuser=0
+    // https://www.testingdocs.com/cucumber-java-bdd-framework/
+    // https://github.com/reportportal/agent-java-cucumber/tree/develop/src/test/resources/features
+
     private final Logger log = LoggerFactory.getLogger(GoogleSearchingStepDefinitions.class);
+
+    //private final WebDriver driver = new ChromeDriver();
+
+    private WebDriver driver;
+
+    @Before
+    public void createDriver() {
+        driver = new ChromeDriver();
+    }
+    @After
+    public void quitDriver() {
+        driver.quit();
+    }
 
     @Given("a web browser is on the Google page")
     public void aWebBrowserIsOnTheGooglePage() {
         log.info("Given: a web browser is on the Google page");
+        driver.get("https://duckduckgo.com/");
+
+        String title = driver.getTitle();
+        assertTrue(title.contains("DuckDuckGo"));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        // throw new PendingException();
     }
 
     @When("the search phrase {string} is entered")
     public void theSearchPhraseIsEntered(String phrase) {
         log.info("When: the search phrase '{}' is entered", phrase);
+        WebElement searchBox = driver.findElement(By.name("q"));
+        WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
+
+        searchBox.sendKeys("Selenium");
+        searchButton.click();
+
+        searchBox = driver.findElement(By.name("q"));
+        String value = searchBox.getAttribute("value");
+        assertEquals("Selenium", value);
     }
 
     @Then("results for {string} are shown")
